8f06dc378ae761429eeceff9a58dbb47b314c100
barloc <stave.tx@gmail.com>
2016-07-03 14:21:54 +0300

    added new design of index page

--- client/app/views.py
+++ client/app/views.py
@@ -5,7 +5,7 @@ from models import HostsModel, BackupStorageModel
 from controller import HostController, VmController
 
 
-@app.route('/login', method=['GET'])
+@app.route('/login', methods=['GET'])
 def login():
     pass
 
