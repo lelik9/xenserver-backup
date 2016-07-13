4bf35621d316378b7b256a84f01eb59687035a95
Alex <lelik9@gmail.com>
2016-07-13 15:47:44 +0300

    changed showUserInput function

--- client/app/static/js/pages/sr.js
+++ client/app/static/js/pages/sr.js
@@ -34,18 +34,26 @@ fillHostSrTable();
 
 function showUserInput(){
     var share = document.getElementById('protocolSelect').value;
+    var loginGroup = document.getElementById('loginGroup');
+    var passwordGroup = document.getElementById('passwordGroup');
+    var inputPath = document.getElementById('inputPath');
+    var loginField =  document.getElementById('inputLogin');
+    var passwordField =  document.getElementById('inputPassword');
+
+    loginGroup.style.display = 'none';
+    passwordGroup.style.display = 'none';
+    loginField.required = false;
+    passwordField.required = false;
 
     if (share.localeCompare('smb') == 0){
-        document.getElementById('loginGroup').style.display = 'block';
-        document.getElementById('passwordGroup').style.display = 'block';
-        document.getElementById('inputPath').placeholder="\\\\10.10.10.10\\smb"
+        loginGroup.style.display = 'block';
+        passwordGroup.style.display = 'block';
+        inputPath.placeholder="\\\\10.10.10.10\\smb";
+        loginField.required = true;
+        passwordField.required = true;
     }else if(share.localeCompare('nfs') == 0) {
-        document.getElementById('loginGroup').style.display = 'none';
-        document.getElementById('passwordGroup').style.display = 'none';
-        document.getElementById('inputPath').placeholder="10.10.10.10:/nfs"
+        inputPath.placeholder="10.10.10.10:/nfs"
     }else if(share.localeCompare('folder') == 0) {
-        document.getElementById('loginGroup').style.display = 'none';
-        document.getElementById('passwordGroup').style.display = 'none';
-        document.getElementById('inputPath').placeholder="/backup/path"
+        inputPath.placeholder="/backup/path"
     }
 }
